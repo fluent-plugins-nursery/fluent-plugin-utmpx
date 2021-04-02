39154415505ec125e20377888fb33bfd38a6aab2
Kentaro Hayashi <hayashi@clear-code.com>
2021-04-02 12:02:56 +0900

    Add missing require

--- lib/fluent/plugin/in_utmpx.rb
+++ lib/fluent/plugin/in_utmpx.rb
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require "fluent/version"
 require "fluent/plugin/input"
 require "fluent/plugin/in_tail/position_file"
 require 'fluent/variable_store'
